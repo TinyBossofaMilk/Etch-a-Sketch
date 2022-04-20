0cf0341a90beb7a6d5ac85decf903364304a896a
TinyBossofaMilk <ppcctan@gmail.com>
2022-04-19 21:06:22 -0700

    js starting structure

---
+++ etchasketch.js
@@ -0,0 +1,14 @@
+const body = document.getRootNode();
+const pixelGrid = document.createElement("div");
+const row = document.createElement("div");
+const pixel = document.createElement("div");
+pixel.id = "pixel";
+pixel.addEventListener("hover", () => )
+
+
+for(let i = 0; i < 4; i++)
+{
+    
+}
+
+body.appendChild(pixelGrid);
