23de0225c9253b10842ae56e64dc54e6c72bbf53
TinyBossofaMilk <ppcctan@gmail.com>
2022-04-24 01:23:31 -0700

    reset button

--- etchasketch.js
+++ etchasketch.js
@@ -1,10 +1,12 @@
-let squaresPerSide = 4;
-let pixelLength = 40;
 const borderWidth = 3;
 const spacing = 10;
 
+let pixelLength = 40;
+let darkeningFactor = 10;
+
 const body = document.querySelector("body");
 const pixelGrid = document.createElement("div");
+pixelGrid.id = "pixelgrid";
 const resetButton = document.createElement("button");
 resetButton.textContent = "reset";
 
@@ -13,14 +15,63 @@ function getRandomRGB(additive = 0)
                  (Math.floor(Math.random()*255) + additive) + "," +  
                  (Math.floor(Math.random()*255) + additive) + ")";}
 
-let rowArr = [];
+//let rowArr = [];
 let darkerArr = [];
 
-// function initializeDarkerArr
 
-function makeGrid(squaresPerSide = 4) 
+function deleteCurrentGrid()
+{
+    let pixel = document.querySelector("pixelgrid");
+    body.removeChild(pixel);
+
+    // pixels.forEach((element) => {
+    //     body.removeChild(element);
+    // });
+}
+
+function reinitializeGrid(length)
+{
+    deleteCurrentGrid();
+    makeGrid(length);
+}
+
+function makeGrid(squaresPerSide = 4)
 {
     for(let i = 0; i < squaresPerSide; i++)
+    {
+        
+
+        darkerArr[i] = [];
+        row = document.createElement("div");
+        row.style.display = "flex";
+        
+        for(let j = 0; j < squaresPerSide; j++) 
+        {
+            darkerArr[i][j] = 0;
+            let pixel = document.createElement("div");
+            pixel.style.margin = spacing / 2 + "px";
+            pixel.style.padding = spacing + "px";
+            pixel.id = "pixel";
+            pixel.style.width = pixelLength + "px";
+            pixel.style.height = pixelLength + "px";
+            pixel.style.border = `${borderWidth}px solid black`;
+            pixel.textContent = darkerArr[i][j];
+            
+            pixel.addEventListener("mouseover", () => 
+            { 
+                pixel.style.backgroundColor =  getRandomRGB();
+                darkerArr[i][j] -= darkeningFactor;
+                pixel.textContent = darkerArr[i][j];
+            });
+
+            pixel.addEventListener("mouseout",  () => { pixel.style.backgroundColor = "rgb(" + (255 + darkerArr[i][j]) + "," + (255 + darkerArr[i][j]) + "," + (255 + darkerArr[i][j]) +  ")"; });
+            row.appendChild(pixel);
+        }
+        pixelGrid.appendChild(row);
+    }
+}
+/*
+for(let i = 0; i < squaresPerSide; i++)
     {
         darkerArr[i] = [];
         rowArr[i] = document.createElement("div");
@@ -41,30 +92,46 @@ function makeGrid(squaresPerSide = 4)
             pixel.addEventListener("mouseover", () => 
             { 
                 pixel.style.backgroundColor =  getRandomRGB();
-                darkerArr[i][j] -= 10;
+                darkerArr[i][j] -= darkeningFactor;
                 pixel.textContent = darkerArr[i][j];
             });
+
             pixel.addEventListener("mouseout",  () => { pixel.style.backgroundColor = "rgb(" + (255 + darkerArr[i][j]) + "," + (255 + darkerArr[i][j]) + "," + (255 + darkerArr[i][j]) +  ")"; });
             rowArr[i].appendChild(pixel);
         }
-    }
-}
+*/
 
 makeGrid();
-//resetButton.addEventListener("onclick", makeGrid(6));
+
+
+resetButton.addEventListener("click", () => {
+
+    deleteCurrentGrid();
+    /*
+    let num = window.prompt("yo sexy");
+    num = parseFloat(num);
+    if(typeof num == "number")
+    {
+        if(num > 100)
+            {makeGrid(100);}
+        else
+            makeGrid(num);
+    }
+    else
+        makeGrid();
+
+    rowArr.forEach((row)=> 
+        {body.appendChild(row);}
+    );*/
+});
+
 body.appendChild(resetButton);
-rowArr.forEach((row)=> 
-    {body.appendChild(row);}
-);
+body.appendChild(pixelGrid);
 
-//body.appendChild(pixelGrid);
+// rowArr.forEach((row)=> 
+//     {body.appendChild(row);}
+// );
 
-/*rowArr[i][j] = document.createElement("div");
-        rowArr[i][j].id = "pixel";
-        rowArr[i][j].style.borderColor = "red";
-        rowArr[i][j].addEventListener("hover", () => {pixel.style.borderColor = "black";})
-        rowArr[i][j].textContent = "box";
-        row.appendChild(rowArr[i][j]);
 /*
 Follow the instructions atop Odin’s Recipes project to set up a Git repository for this project.
 Create a webpage with a 16x16 grid of square divs.
